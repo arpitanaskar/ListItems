a5e37bccdb74875462694af72484cdfea35db733
arpitanaskar <arpitanaskarphone@gmail.com>
2023-07-01 11:52:26 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// let mainHeader = document.getElementById("main-header");
+
+// mainHeader.style.color = "red"
+
+let header = document.getElementById('main-header');
+let headerTitle = document.getElementById('header-title');
+header.style.borderBottom = "solid 3px #000"
+
+let addItems = document.getElementById("add-items");
+addItems.style.fontWeight = "bold";
+addItems.style.color = "green"

1ca9ea864327ecc4309a7a1ada4a4c64857186aa
arpitanaskar <arpitanaskarphone@gmail.com>
2023-07-02 17:42:41 +0530

    QuerySelector

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// let items = document.getElementsByClassName('list-group-item');
-// for (let i=0; i<items.length; i++) {
-//     items[i].style.fontWeight = "bold";
-// }
+// 
 
-let fifth = document.getElementsByClassName('fifth');
-// fifth.style.color = "red"
+document.querySelector('.list-group-item:nth-child(2)').style.backgroundColor = "green";
+document.querySelector('.list-group-item:nth-child(3)').style.display = "none";
 
-let li = document.getElementsByTagName('li');
-li[4].style.color = "red";
+
+let list = document.querySelectorAll('.list-group-item');
+list[1].style.color = "green"
+
+for (let i=0; i<list.length; i+=2) { 
+        list[i].style.backgroundColor = "green";
+}

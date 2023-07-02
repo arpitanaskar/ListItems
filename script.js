36a61e9fdb93720a6d23bb04cff258e9b2dee867
arpitanaskar <arpitanaskarphone@gmail.com>
2023-07-02 18:22:25 +0530

    ParentChildNodes

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-// 
+let newDiv = document.createElement('div');
+let text = document.createTextNode("HEllo");
+newDiv.appendChild(text);
 
-document.querySelector('.list-group-item:nth-child(2)').style.backgroundColor = "green";
-document.querySelector('.list-group-item:nth-child(3)').style.display = "none";
 
 
-let list = document.querySelectorAll('.list-group-item');
-list[1].style.color = "green"
 
-for (let i=0; i<list.length; i+=2) { 
-        list[i].style.backgroundColor = "green";
-}
+let container = document.querySelector('header .container');
+let h1 = document.querySelector('header h1');
+
+container.insertBefore(newDiv, h1);
+
+
+let secDiv = document.createElement('div');
+let secDivText = document.createTextNode("HEllo");
+secDiv.appendChild(secDivText);
+
+let listGroup = document.querySelector('.list-group'); // Select the container directly
+let item1 = document.querySelectorAll('.list-group-item')[0];
+
+listGroup.insertBefore(secDiv, item1);

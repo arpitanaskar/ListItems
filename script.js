30cfeb938b0d79891776641b73f4d8a837bbd5b6
arpitanaskar <arpitanaskarphone@gmail.com>
2023-07-03 10:17:38 +0530

    DeleteEditButton

--- script.js
+++ script.js
@@ -1,21 +1,71 @@
-let newDiv = document.createElement('div');
-let text = document.createTextNode("HEllo");
-newDiv.appendChild(text);
+let form = document.getElementById('addForm');
+let itemList = document.getElementById('items');
+
+//Form Submit Event 
+form.addEventListener('submit', addItem);
+
+//Remove Event
+itemList.addEventListener('click', removeItem);
+
+function addItem(e) {
+    e.preventDefault();
+
+    //get Input value
+    var newItem = document.getElementById('item').value;
+
+    //create li element
+    let li = document.createElement('li');
+
+    //create delete button
+    let delButton = document.createElement('button');
+
+    //add class to the button
+    delButton.className = 'btn btn-danger btn-sm float-right delete'
+
+    //adding X into del button
+    delButton.appendChild(document.createTextNode('X'));
+
+    let editButton = document.createElement('button');
+
+    editButton.className = 'btn btn-sm float-right'
+
+    editButton.appendChild(document.createTextNode('Edit'));
+
+    //li class
+    li.className = 'list-group-item';
+
+    li.appendChild(document.createTextNode(newItem));
+
+    li.appendChild(delButton)
+
+    li.appendChild(editButton)
+
+    itemList.appendChild(li);
+}
+
+
+//function to remove an item
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are You Sure?')) {
+            let li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
+//Edit Button
+let items = document.getElementsByClassName('list-group-item');
 
 
 
+for (let i = 0; i < items.length; i++) {
+    //creating Edit element
+    let editButton = document.createElement('button');
 
-let container = document.querySelector('header .container');
-let h1 = document.querySelector('header h1');
+    editButton.className = 'btn btn-sm float-right'
 
-container.insertBefore(newDiv, h1);
-
-
-let secDiv = document.createElement('div');
-let secDivText = document.createTextNode("HEllo");
-secDiv.appendChild(secDivText);
-
-let listGroup = document.querySelector('.list-group'); // Select the container directly
-let item1 = document.querySelectorAll('.list-group-item')[0];
-
-listGroup.insertBefore(secDiv, item1);
+    //adding text into it
+    editButton.appendChild(document.createTextNode('Edit'));
+    items[i].appendChild(editButton);
+}
